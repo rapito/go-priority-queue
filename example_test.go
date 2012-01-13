e5d6797c424e4c3b8e35a73dad9f69b007543574
Stefan Nilsson <trolleriprofessorn@gmail.com>
2012-01-13 21:06:44 +0100

    Add godoc example.

---
+++ example_test.go
@@ -0,0 +1,38 @@
+// Copyright 2012 Stefan Nilsson
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package prio_test
+
+import (
+	"fmt"
+	"prio"
+)
+
+// 0234
+func ExampleQueue() {
+	// myType implements prio.Interface as shown above in the package overview.
+	a := make([]*myType, 5)
+	q := prio.New()
+
+	// Create and push elements with values 0 to 4 onto the queue.
+	for i := range a {
+		a[i] = &myType{value: i}
+		q.Push(a[i])
+	}
+
+	q.Remove(a[1].index) // Use index to find and remove element with value 1.
+	for q.Len() > 0 {
+		fmt.Print(q.Pop().(*myType).value)
+	}
+}
